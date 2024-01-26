7e05c07579fe7746bdefffd9be5bd0ec0341b289
Stefano Rubinetti <stefano.rubinetti@gmail.com>
2024-01-26 10:11:27 +0100

    v

--- src/main/java/com/generation/primarestapi/controllers/PersonController.java
+++ src/main/java/com/generation/primarestapi/controllers/PersonController.java
@@ -4,18 +4,24 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.primarestapi.model.entities.Person;
 import com.generation.primarestapi.model.repositories.PersonRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 
 
 
 @RestController
+@CrossOrigin
 public class PersonController 
 {
     @Autowired
@@ -31,20 +37,40 @@ public class PersonController
         List<Person> people = repo.findAll();
         return people;
     }
-    
     //inserimento è POST
-
     //La request contiene un JSON, il json della persona da inserire
     //con RequestBody Spring lo prende, lo traduce in un oggetto Person
     //chiamo newPerson
     //@RequestBody è per RestController quello che @ModelAttribute è per il controller
     //normale 
     @PostMapping("/people")
-    @CrossOrigin
+   
     public Person  insertPerson(@RequestBody Person newPerson) 
     {
         Person salvata= repo.save(newPerson);
         return salvata;
+    }
+    
+    @GetMapping("/people/{id}")//{id} che è un segnaposto, in automatico quello che c'è dopo lo / viene messo nella variabile id
+    //localhost:8080/people/6    -> spring vede che dove c'è il placeholder {id} ora c'è 6 e lo mette nel parametro id
+    public Person getPerson(@PathVariable Integer id) 
+    {
+        return repo.findById(id).get();
+    }
+
+    @PutMapping("/people/{id}")
+    public Person updatePerson(@PathVariable Integer id,@RequestBody Person newVersion) 
+    {
+        //ci arriva il JSON di una Person, che contiene i valori modificati di quella persona
+        //ci arriva tratime path variabile il suo id
+        newVersion.setId(id);
+        return repo.save(newVersion);
+    }
+
+    @DeleteMapping("/people/{id}")
+    public void deletePerson(@PathVariable Integer id)//void manda indietro una request senza body
+    {
+        repo.deleteById(id);
     }
     
 }
--- src/main/java/com/generation/primarestapi/model/entities/Person.java
+++ src/main/java/com/generation/primarestapi/model/entities/Person.java
@@ -2,10 +2,12 @@ package com.generation.primarestapi.model.entities;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,15 +16,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-@Entity
 @Builder
+@Entity
 public class Person 
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private String name,surname,job;
+    private String name;
+    private String surname;
+    private String job;
 
     private LocalDate dob;
+
 }
